50e785f9bf0519cff76e0cfb5f342b9d2c7f68aa
cheekysim <minecraftcheekysim@gmail.com>
2023-05-09 21:25:39 +0100

    updated gulp and api path

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require('gulp');
 
-gulp.task('default', () => {
-	return gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
+gulp.task('css', () => {
+        return gulp.src('src/**/*.css').pipe(gulp.dest('dist'));
 });
+
+gulp.task('views', () => {
+        return gulp.src('src/**/*.ejs').pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+        return gulp.src('src/**/*.webp').pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', gulp.parallel('css', 'views', 'images'));
